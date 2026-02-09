4e586fb48d9e7e25744be8918677e5e705df114c
siyul3330 <siyul3330@gmail.com>
2026-02-09 11:48:24 +0900

    [생성] 사용자가 정의한 useState()

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import Register from './components/Register';
+import HookExam from './components/HookExam';
 
 export default function App() {
 
   return <>
-    <Register />
+    <HookExam />
   </>
 }
